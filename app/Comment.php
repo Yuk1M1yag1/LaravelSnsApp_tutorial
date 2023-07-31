6f70443b2a1ac10d370f814c797f6d6ac3145593
EC2 Default User <ec2-user@ip-172-31-7-106.ap-northeast-1.compute.internal>
2023-07-31 15:12:07 +0000

    first commit

---
+++ app/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    // 「１対１」→ メソッド名は単数形
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    
+    // 「１対１」→ メソッド名は単数形
+    public function post()
+    {
+      return $this->belongsTo('App\Post');
+    }
+}
